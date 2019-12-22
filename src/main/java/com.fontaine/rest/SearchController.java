2b139b68b4c2bb47b2ef72f671336c5e692e7d9b
Fazel Farnia <fazel.farnia@gmail.com>
2019-12-22 21:41:40 +0330

    Add rest controller layer with endpoints

---
+++ src/main/java/com.fontaine/rest/SearchController.java
@@ -0,0 +1,43 @@
+package com.fontaine.rest;
+
+import com.fontaine.model.Author;
+import com.fontaine.model.Contributor;
+import com.fontaine.model.SearchResponse;
+import com.fontaine.service.SearchRepositoryServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Fazel on 12/22/2019.
+ */
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+
+    @Autowired
+    private SearchRepositoryServiceImpl repositoryService;
+
+    @GetMapping(value = "/query/{query}")
+    public SearchResponse SearchByNameInPublicRepository(@PathVariable(name = "query") String query) {
+        return repositoryService.searchPublicRepo(query);
+    }
+
+    @GetMapping(value = "/contributors")
+    public List<Contributor> getAllContributor(@RequestParam(name = "url") String contributorUrl) {
+        return repositoryService.getCommitters(contributorUrl);
+    }
+
+    @GetMapping(value = "/commits/number")
+    public Map<String, Long> committerActivity(@RequestParam(name = "url") String commitUrl) {
+        return repositoryService.committerActivity(commitUrl.replace("{/sha}", ""));
+    }
+
+    @GetMapping(value = "/commits/timeline")
+    public List<Author> committerTimeline(@RequestParam(name = "url") String commitUrl) {
+        return repositoryService.projectionTimline(commitUrl.replace("{/sha}", ""));
+    }
+}
