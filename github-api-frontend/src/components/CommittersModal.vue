56d13a9acf658f2207f8c5d305229da1b5206436
Fazel Farnia <fazel.farnia@gmail.com>
2019-12-24 13:00:19 +0330

    Add Committers page to show list of all contributors

---
+++ github-api-frontend/src/components/CommittersModal.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-modal id="committers" size="xl" title="List of Committers">
+        <div class="wrapper">
+            <b-table striped hover :items="committersData">
+                </b-table>
+        </div>
+    </b-modal>
+</template>
+
+<script>
+
+    export default {
+        name: 'CommittersModal',
+props: {
+    committersData: Array
+},
+        data: function () {
+            return {
+                item: ''
+            }
+        },
+
+        computed: {
+            itemToShow: function () {
+                return this.committers
+            }
+        },
+
+        methods: {
+
+    }
+    }
+</script>
