1a7ad65bfc76a256d19babb5c3fae233149f3ea4
Fazel Farnia <fazel.farnia@gmail.com>
2019-12-25 11:25:10 +0330

    Add echart and timeline dependencies

--- github-api-frontend/src/main.js
+++ github-api-frontend/src/main.js
@@ -5,11 +5,19 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import BootstrapVue from 'bootstrap-vue'
 import { VBModal } from 'bootstrap-vue'
+import ECharts from 'vue-echarts'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/component/tooltip'
+import LightTimeline from 'vue-light-timeline';
 
 Vue.config.productionTip = false
 
+
 Vue.directive('b-modal', VBModal)
+Vue.use(LightTimeline);
 Vue.use(BootstrapVue);
+Vue.component('v-chart', ECharts)
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
